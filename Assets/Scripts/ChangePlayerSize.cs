bcb53d4349921fe9102cccc26f20088f6b8cad2d
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-08-17 01:56:04 +0200

    Font and better size change

--- Assets/Scripts/ChangePlayerSize.cs
+++ Assets/Scripts/ChangePlayerSize.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class ChangePlayerSize : MonoBehaviour
 {
     private float _speed = 0f;
-    private float _minSpeed = 0.02f;
+    private float _minSpeed = 0.05f;
+    private float _curveTime = 2f;
+
+    [Header("Speed changing curve")]
+    [SerializeField] AnimationCurve speedCurve;
 
     private void FixedUpdate()
     {
@@ -26,10 +30,13 @@ public class ChangePlayerSize : MonoBehaviour
     private IEnumerator ChangeSpeedSmooth(float initialSpeed)
     {
         int direction = initialSpeed < 0 ? -1 : 1;
+        float elapsedTime = 0;
         _speed = initialSpeed;
         while (Mathf.Abs(_speed) > _minSpeed)
         {
-            _speed = Mathf.Lerp(_speed, _minSpeed * direction, Time.fixedDeltaTime * 1.2f);
+            elapsedTime += Time.deltaTime;
+            float percentageCompleted = elapsedTime / _curveTime;
+            _speed = Mathf.Lerp(initialSpeed, _minSpeed * direction, speedCurve.Evaluate(percentageCompleted));
             yield return new WaitForFixedUpdate();
         }
         _speed = _minSpeed * direction;
