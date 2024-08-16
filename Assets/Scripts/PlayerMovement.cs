fba74bc5233216164bc28c183f9f3f54b8007d6c
Kasia Jaromirska <kasia.jaromirska@gmail.com>
2024-08-16 20:42:23 +0200

    Added basic 2D Top Down movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] float speed = 3f;
+
+    private Rigidbody2D _rb;
+    private Vector2 _velocity;
+
+    void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+    }
+
+    private void OnMove(InputValue value)
+    {
+        _velocity = value.Get<Vector2>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        _rb.MovePosition(_rb.position + _velocity * speed * Time.deltaTime);
+        Debug.Log(_velocity);
+    }
+}
